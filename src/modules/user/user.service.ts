a2ad4c5d193a2d5314b171d8c98ac70c9dfb1b5e
fresemin <fresemin@gmail.com>
2024-03-07 14:43:01 +0300

    feat: find all users

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { UserEntity } from '../db/entities/user.entity';
 
 @Injectable()
 export class UserService {
+  constructor(private readonly userEntity: UserEntity) {}
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
 
   findAll() {
-    return `This action returns all user`;
+    return this.userEntity.findAll();
   }
 
   findOne(id: number) {
