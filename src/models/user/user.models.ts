9f3e0677c5e92278ac2a61f13601bce95f49bb37
fresemin <fresemin@gmail.com>
2024-03-07 15:48:14 +0300

    feat: user class model

--- src/models/user/user.models.ts
+++ src/models/user/user.models.ts
@@ -1,3 +1,5 @@
+import { v4 as uuidv4 } from 'uuid';
+
 export interface IUser {
   id: string;
   login: string;
@@ -5,4 +7,26 @@ export interface IUser {
   version: number;
   createdAt: number;
   updatedAt: number;
+}
+
+export class User implements IUser {
+  id: string = uuidv4();
+
+  login: string;
+
+  password: string;
+
+  version: number = 0;
+
+  createdAt: number = Date.now();
+
+  updatedAt: number;
+
+  constructor(user: Partial<IUser>) {
+    Object.assign(this, user);
+
+    this.version += 1;
+
+    this.updatedAt = Date.now();
+  }
 }
