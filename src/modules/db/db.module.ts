fb74c0c707bd5704d898e4855083ec3d2121288c
fresemin <fresemin@gmail.com>
2024-03-08 22:55:22 +0300

    feat: init favorites entity

--- src/modules/db/db.module.ts
+++ src/modules/db/db.module.ts
@@ -3,9 +3,22 @@ import { UserEntity } from './entities/user/user.entity';
 import { ArtistEntity } from './entities/artist/artist.entity';
 import { TrackEntity } from './entities/track/track.entity';
 import { AlbumEntity } from './entities/album/album.entity';
+import { FavoritesEntity } from './entities/favorites/favorites.entity';
 
 @Module({
-  providers: [UserEntity, ArtistEntity, TrackEntity, AlbumEntity],
-  exports: [UserEntity, ArtistEntity, TrackEntity, AlbumEntity],
+  providers: [
+    UserEntity,
+    ArtistEntity,
+    TrackEntity,
+    AlbumEntity,
+    FavoritesEntity,
+  ],
+  exports: [
+    UserEntity,
+    ArtistEntity,
+    TrackEntity,
+    AlbumEntity,
+    FavoritesEntity,
+  ],
 })
 export class DBModule {}
