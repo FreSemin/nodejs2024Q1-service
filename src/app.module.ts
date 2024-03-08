2efe0f3099042b4e9455a43b73f7e1b226902d6e
fresemin <fresemin@gmail.com>
2024-03-08 13:54:06 +0300

    feat: init basic track module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './modules/user/user.module';
 import { ArtistModule } from './modules/artist/artist.module';
+import { TrackModule } from './modules/track/track.module';
 
 @Module({
-  imports: [UserModule, ArtistModule],
+  imports: [UserModule, ArtistModule, TrackModule],
   controllers: [],
   providers: [],
 })
