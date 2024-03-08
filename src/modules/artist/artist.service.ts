9f0f25fc65188fccdc16a7b038b8a4c9d79da6e4
fresemin <fresemin@gmail.com>
2024-03-08 22:50:08 +0300

    feat: set null for albums and tracks on delete artist

--- src/modules/artist/artist.service.ts
+++ src/modules/artist/artist.service.ts
@@ -2,11 +2,17 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateArtistDto } from './dto/create-artist.dto';
 import { UpdateArtistDto } from './dto/update-artist.dto';
 import { ArtistEntity } from '../db/entities/artist/artist.entity';
-import { Artist } from 'src/models';
+import { Album, Artist, Track } from 'src/models';
+import { AlbumEntity } from '../db/entities/album/album.entity';
+import { TrackEntity } from '../db/entities/track/track.entity';
 
 @Injectable()
 export class ArtistService {
-  constructor(private readonly artistEntity: ArtistEntity) {}
+  constructor(
+    private readonly artistEntity: ArtistEntity,
+    private readonly albumEntity: AlbumEntity,
+    private readonly trackEntity: TrackEntity,
+  ) {}
 
   create(createArtistDto: CreateArtistDto): Artist {
     return this.artistEntity.create(createArtistDto);
@@ -36,9 +42,27 @@ export class ArtistService {
     });
   }
 
-  // TODO: on delete set null for artist tracks and albums
   remove(id: string) {
     this.findOne(id);
+
+    // TODO: refactor using Prisma
+    const artistAlbums: Album[] = this.albumEntity.findAllByArtistId(id);
+
+    artistAlbums.forEach((album) => {
+      this.albumEntity.update(album.id, {
+        ...album,
+        artistId: null,
+      });
+    });
+
+    const artistTracks: Track[] = this.trackEntity.findAllByArtistId(id);
+
+    artistTracks.forEach((track) => {
+      this.trackEntity.update(track.id, {
+        ...track,
+        artistId: null,
+      });
+    });
 
     this.artistEntity.remove(id);
   }
