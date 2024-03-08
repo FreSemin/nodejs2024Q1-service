59e385c900d3dd1f91c1d6ccce19215abba2eb85
fresemin <fresemin@gmail.com>
2024-03-08 23:22:55 +0300

    feat: favorites get all

--- src/modules/db/entities/favorites/favorites.entity.ts
+++ src/modules/db/entities/favorites/favorites.entity.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { Favorites } from 'src/models';
 
 @Injectable()
-export class FavoritesEntity {}
+export class FavoritesEntity {
+  private favorites: Favorites = new Favorites({});
+
+  constructor() {}
+
+  findAll() {
+    return this.favorites;
+  }
+}
--- src/modules/favorites/favorites.service.ts
+++ src/modules/favorites/favorites.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { Favorites } from 'src/models';
+import { FavoritesEntity } from '../db/entities/favorites/favorites.entity';
 
 @Injectable()
-export class FavoritesService {}
+export class FavoritesService {
+  constructor(private readonly favoritesEntity: FavoritesEntity) {}
+
+  findAll(): Favorites {
+    return this.favoritesEntity.findAll();
+  }
+}
