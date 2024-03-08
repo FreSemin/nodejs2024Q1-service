f277cc2f93a6e9b6915c94c0032b5ff4f9230efc
fresemin <fresemin@gmail.com>
2024-03-08 22:47:19 +0300

    fix: create track dto relations

--- src/modules/track/dto/create-track.dto.ts
+++ src/modules/track/dto/create-track.dto.ts
@@ -1,15 +1,11 @@
-import { IsNotEmpty, IsNumber, IsUUID } from 'class-validator';
+import { IsNotEmpty, IsNumber } from 'class-validator';
 
 export class CreateTrackDto {
   @IsNotEmpty()
   name: string;
 
-  @IsNotEmpty()
-  @IsUUID('4')
   artistId: string;
 
-  @IsNotEmpty()
-  @IsUUID('4')
   albumId: string;
 
   @IsNotEmpty()
