eefc5e65eec5ec9c98ee9f8fa7eea7efa1c0b159
fresemin <fresemin@gmail.com>
2024-03-08 15:02:46 +0300

    feat: album entity

--- src/modules/db/db.module.ts
+++ src/modules/db/db.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserEntity } from './entities/user/user.entity';
 import { ArtistEntity } from './entities/artist/artist.entity';
 import { TrackEntity } from './entities/track/track.entity';
+import { AlbumEntity } from './entities/album/album.entity';
 
 @Module({
-  providers: [UserEntity, ArtistEntity, TrackEntity],
-  exports: [UserEntity, ArtistEntity, TrackEntity],
+  providers: [UserEntity, ArtistEntity, TrackEntity, AlbumEntity],
+  exports: [UserEntity, ArtistEntity, TrackEntity, AlbumEntity],
 })
 export class DBModule {}
---
+++ src/modules/db/entities/album/album.entity.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@nestjs/common';
+import { Album } from 'src/models';
+import { CreateAlbumDto } from 'src/modules/album/dto/create-album.dto';
+
+@Injectable()
+export class AlbumEntity {
+  private album: Album[] = [];
+
+  constructor() {}
+
+  create(createAlbumDto: CreateAlbumDto): Album {
+    const album: Album = new Album(createAlbumDto);
+
+    this.album.push(album);
+
+    return album;
+  }
+
+  findAll(): Album[] {
+    return this.album;
+  }
+
+  findOne(id: string): Album | null {
+    return this.album.find((album) => album.id === id) || null;
+  }
+
+  update(id: string, updatedAlbum: Album): Album | null {
+    const albumIndex: number = this.album.findIndex((album) => album.id === id);
+
+    if (albumIndex !== -1) {
+      this.album[albumIndex] = new Album(updatedAlbum);
+
+      return this.album[albumIndex];
+    }
+
+    return null;
+  }
+
+  remove(id: string): void {
+    const albumIndex: number = this.album.findIndex((album) => album.id === id);
+
+    if (albumIndex !== -1) {
+      this.album.splice(albumIndex, 1);
+
+      return;
+    }
+  }
+}
