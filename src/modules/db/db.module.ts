c348489340d6f3b529bbf7042bb752e01286f6f2
fresemin <fresemin@gmail.com>
2024-03-08 13:54:14 +0300

    feat: track entity

--- src/modules/db/db.module.ts
+++ src/modules/db/db.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserEntity } from './entities/user/user.entity';
 import { ArtistEntity } from './entities/artist/artist.entity';
+import { TrackEntity } from './entities/track/track.entity';
 
 @Module({
-  providers: [UserEntity, ArtistEntity],
-  exports: [UserEntity, ArtistEntity],
+  providers: [UserEntity, ArtistEntity, TrackEntity],
+  exports: [UserEntity, ArtistEntity, TrackEntity],
 })
 export class DBModule {}
---
+++ src/modules/db/entities/track/track.entity.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@nestjs/common';
+import { Track } from 'src/models';
+import { CreateTrackDto } from 'src/modules/track/dto/create-track.dto';
+
+@Injectable()
+export class TrackEntity {
+  private track: Track[] = [];
+
+  constructor() {}
+
+  create(createTrackDto: CreateTrackDto): Track {
+    const track: Track = new Track(createTrackDto);
+
+    this.track.push(track);
+
+    return track;
+  }
+
+  findAll(): Track[] {
+    return this.track;
+  }
+
+  findOne(id: string): Track | null {
+    return this.track.find((track) => track.id === id) || null;
+  }
+
+  update(id: string, updatedTrack: Track): Track | null {
+    const trackIndex: number = this.track.findIndex((track) => track.id === id);
+
+    if (trackIndex !== -1) {
+      this.track[trackIndex] = new Track(updatedTrack);
+
+      return this.track[trackIndex];
+    }
+
+    return null;
+  }
+
+  remove(id: string): void {
+    const trackIndex: number = this.track.findIndex((track) => track.id === id);
+
+    if (trackIndex !== -1) {
+      this.track.splice(trackIndex, 1);
+
+      return;
+    }
+  }
+}
