0eb9c5b11ab84eeb4394a014dee795cd349e447d
fresemin <fresemin@gmail.com>
2024-03-09 17:14:10 +0300

    feat: start app on port from env

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const configService = app.get(ConfigService);
+
+  // TODO: add default port to constants
+  const port = configService.get<number>('PORT') || 4000;
+
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(4000);
+  await app.listen(port).then(() => {
+    // TODO: add message to constants
+    console.log(`App listening on port: ${port}`);
+  });
 }
 bootstrap();
