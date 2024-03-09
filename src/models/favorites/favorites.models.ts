632bf531c1e046e75e66a118716e9749c8b40df8
fresemin <fresemin@gmail.com>
2024-03-09 13:12:30 +0300

    feat: favorites response model

--- src/models/favorites/favorites.models.ts
+++ src/models/favorites/favorites.models.ts
@@ -1,3 +1,7 @@
+import { Album } from '../album/album.models';
+import { Artist } from '../artist/artist.models';
+import { Track } from '../track/track.models';
+
 export interface IFavorites {
   artists: string[];
   albums: string[];
@@ -11,5 +15,21 @@ export class Favorites implements IFavorites {
 
   constructor(favorites: Partial<IFavorites>) {
     Object.assign(this, favorites);
+  }
+}
+
+export interface IFavoritesResponse {
+  artists: Artist[];
+  albums: Album[];
+  tracks: Track[];
+}
+
+export class FavoritesResponse implements IFavoritesResponse {
+  artists: Artist[] = [];
+  albums: Album[] = [];
+  tracks: Track[] = [];
+
+  constructor(favoritesResponse: Partial<IFavoritesResponse>) {
+    Object.assign(this, favoritesResponse);
   }
 }
