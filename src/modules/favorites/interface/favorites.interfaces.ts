784d2c47f6e8ed5d2ef838a0e496c685162d23ba
fresemin <fresemin@gmail.com>
2024-03-15 16:16:48 +0300

    ref: delete unused favorites interface

--- src/modules/favorites/interface/favorites.interfaces.ts
+++ src/modules/favorites/interface/favorites.interfaces.ts
@@ -7,12 +7,6 @@ import {
   Track,
 } from '@prisma/client';
 
-export interface Favorites {
-  artists: string[];
-  albums: string[];
-  tracks: string[];
-}
-
 export interface FavoritesResponse {
   artists: Artist[];
   albums: Album[];
